526de9e2650aaaa8c506874155e56e7a657aad3c
04divya <136967050+04divya@users.noreply.github.com>
2025-08-15 04:00:04 +0800

    classification.py

--- utils/classification.py
+++ utils/classification.py
@@ -1,12 +1,8 @@
 def classify_document(text):
     text = text.lower()
-    if "course content" in text:
-        return "Course Content Document"
-    elif "assessment" in text:
-        return "Assessment Document"
-    elif "syllabus" in text:
-        return "Syllabus Document"
-    elif "grading" in text:
-        return "Grading Document"
+    if "transcript" in text or "grade" in text or "gpa" in text or "credit" in text:
+        return "Student Result / Transcript Document"
+    elif "compulsory courses" in text or "programme structure" in text or "credit hours" in text:
+        return "Programme Structure Document"
     else:
         return "Unclassified Document"
